838d58136100d09cb982a6f24f049e0e80fad03a
amirdotnetdev <amirkamyabi4@gmail.com>
2024-02-09 11:24:51 -0500

    commit

--- item/models.py
+++ item/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -11,3 +11,16 @@ class Category(models.Model):
     def __str__(self):
         return self.name
         
+class Item(models.Model):
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 255)
+    description = models.TextField(max_length = 255, blank = True, null = True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_image', blank=True, null=True)
+    is_sold = models.BooleanField(default = False)
+    created_by = models.ForeignKey(User, on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
