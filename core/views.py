55b0b727b32ea9cfdb15e3e828b73a88447d9f97
amirdotnetdev <amirkamyabi4@gmail.com>
2024-02-10 00:27:33 -0500

    commit

--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from item.models import *
 def index(request):
-    return render(request, 'core/index.html')
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold= False)
+    
+    context = {
+        
+        'items' : items,
+        'categories' : categories
+               }
+    return render(request, 'core/index.html', context=context ) 
 def contact(request):
     return render(request, 'core/contact.html')
